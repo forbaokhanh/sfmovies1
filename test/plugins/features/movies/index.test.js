dc974007aa2274b0882b7688eca1800f2c94651b
tranthibaokhanh <tranthibaokhanh@gmail.com>
2017-05-25 15:25:12 -0700

    feat(get-movies): creates a get endpoint for retrieving a list of all movies, movies with a certain year, title, in a year range and a fuzzy title matching.

--- test/plugins/features/movies/index.test.js
+++ test/plugins/features/movies/index.test.js
@@ -20,4 +20,40 @@ describe('movies integration', () => {
 
   });
 
+  describe('display', () => {
+
+    it('displays all movies', () => {
+      return Movie.inject({
+        url: '/movies',
+        method: 'GET',
+      })
+      .then((response) => {
+        expect(response.statusCode).to.eql(200);
+      });
+    });
+
+    it('displays movies in a specified release year', () => {
+      return Movie.inject({
+        url: '/movies',
+        method: 'GET',
+        headers: { release_year: '2005' }
+      })
+      .then((response) => {
+        expect(response.statusCode).to.eql(200);
+      });
+    });
+
+    it('displays movies in that matches a title', () => {
+      return Movie.inject({
+        url: '/movies',
+        method: 'GET',
+        headers: { title: 'WALL-E' }
+      })
+      .then((response) => {
+        expect(response.statusCode).to.eql(200);
+      });
+    });
+
+  });
+
 });
